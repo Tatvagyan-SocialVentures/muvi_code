f8958749f2c35e096326dbdb9f4bb3b8f8a72042
ThinkZone <89777814+thinkzoneDev@users.noreply.github.com>
2024-11-28 18:36:14 +0530

    data path changed

--- src/services/service.js
+++ src/services/service.js
@@ -1,3 +1,7 @@
+import jsonData from "../data/all_file_list.json";
+
+// get data from GitHub
+/*
 export const getMoviesData = async () => {
   try {
     //const endPoint = "https://raw.githubusercontent.com/nayak001/mymovies/refs/heads/main/all_file_list.json";
@@ -8,6 +12,17 @@ export const getMoviesData = async () => {
       throw new Error("Failed to fetch data");
     }
     const jsonData = await response.json();
+    return jsonData;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return [];
+  }
+};
+*/
+
+// get data from local
+export const getMoviesData = async () => {
+  try {
     return jsonData;
   } catch (error) {
     console.error("Error fetching data:", error);
